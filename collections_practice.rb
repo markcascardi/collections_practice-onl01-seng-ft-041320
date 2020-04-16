10017a59aa8fe929fc265993a098922a473dd674
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 07:42:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -21,15 +21,8 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  i = 0
-
-  while i < array.length
-    yield(array[i])
-    i += 1
+  array.each do |word|
+    word[2] = $
+    array
   end
-
-kesha_maker(array)
-  array.map do |word| word[2].chr = "$"
-  end
-  array
 end
