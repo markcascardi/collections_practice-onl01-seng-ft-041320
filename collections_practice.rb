a209389f82c6e5ff1cca6d717bfca6c8669102b4
Mark Cascardi <mcascardi@gmail.com>
2020-04-16 06:45:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -7,6 +7,6 @@ def sort_array_desc(integers)
   integers.sort.reverse
 end
 
-def sort_array_char_count(integers)
-  integers.length.sort
+def sort_array_char_count(array)
+  array.map { |word| word.length.sort }
 end
